9b9f0240efbc2fca2032cc8e9cde6ac7d26b7ee5
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2025-11-26 18:16:39 +0800

    Done with temp conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+  temp = (temp - 32) * 5 /9;
+  let roundedTemp = Math.round(temp * 10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  temp = (temp * 9 / 5) + 32;
+  let roundedTemp = Math.round(temp * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
