1f986a0643fa4fcac6377bd96fd8760b7e8a8aca
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2025-11-26 17:36:13 +0800

    done with sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let temp = 0;
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return 'ERROR';
+    }
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+    if (min > max) {
+        temp = min; //temp now has value min no more
+        min = max; //min now has the value of max
+        max = temp;
+    }
+let sum = 0;
+for (let i = min; i <= max; i++) {
+    sum += i;   
+}
+return sum;
 };
 
 // Do not edit below this line
